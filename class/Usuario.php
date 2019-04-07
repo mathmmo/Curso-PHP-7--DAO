9640b9520aaca7e298d3d1f8ee36d94be76fe148
mathmmo <matheusm.oliveira1995@gmail.com>
2019-04-07 00:08:01 -0300

    Continuação das aulas

--- class/Usuario.php
+++ class/Usuario.php
@@ -1,10 +1,12 @@
 <?php
 class Usuario {
+	//Atributos
 	private $idusuario;
 	private $deslogin;
 	private $dessenha;
 	private $dtcadastro;
 
+	//Metodos Seters
 	public function setUsuario($id){
 		$this->idusuario = $id;
 	}
@@ -21,6 +23,7 @@ class Usuario {
 		$this->dtcadastro = $value;
 	}
 
+	//Metodos Geters
 	public function getIdusuario(){
 		return $this->idusuario;
 	}
@@ -37,10 +40,10 @@ class Usuario {
 		return $this->dtcadastro;
 	}
 
+	//Carregar por ID
 	public function loadById($id){
 		$sql = new Sql();
 		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario =:ID",array(":ID"=>$id));
-
 		if (isset($results[0])){
 			$row = $results[0];
 			$this->setUsuario($row['idusuario']);
@@ -49,6 +52,35 @@ class Usuario {
 			$this->setDtcadastro(new DateTime($row['dtcadastro']));
 		}
 	}
+
+	//Lista Geral
+	static function getList(){
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+	}
+
+	//Procurar por %.*.%
+	static function search($login){
+		$sql = new Sql();
+		return $sql->select("SELECT deslogin, dessenha FROM tb_usuarios WHERE deslogin LIKE :LOGIN ORDER BY idusuario",array(":LOGIN"=>"%".$login."%"));
+	}
+
+	//Carregar por login
+	public function login($login, $senha){
+		$sql = new Sql();
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin =:LOGIN AND dessenha = :SENHA",array(":LOGIN"=>$login, ":SENHA"=>$senha));
+		if (isset($results[0])){
+			$row = $results[0];
+			$this->setUsuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		} else {
+			throw new Exception("Login e/ou senha inválidos.");
+		}
+	}
+
+	//Metodo mágico toString
 	public function __toString(){
 		return json_encode(array(
 			"idusuario"=>$this->getIdusuario(),
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once("config.php");
 
-$matheus = new Usuario();
+/*$matheus = new Usuario();
 
 $matheus->loadById(2);
 
@@ -22,4 +22,20 @@ $root = new Usuario();
 $root->loadById(7);
 
 echo $root;
+echo "<br><br>===================================================<br> Lista de usuarios<br>";
+$lista = Usuario::getList();
+
+echo json_encode($lista);
+
+echo "<br><br>===================================================<br> Procurar Usuario<br>";
+$matheus2 = Usuario::search("lo");
+
+echo json_encode($matheus2);
+
+echo "<br><br>===================================================<br> Localizando por Login e senha<br>";*/
+
+//carregar um usuario usando um login e uma senha
+$login  = new Usuario();
+$login->login("LegendOfLegaia","otimorpg");
+echo $login;
 ?>
